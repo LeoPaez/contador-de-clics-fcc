d42c5843684224dcf6fdf7605b9bf01558cd9514
LeoPaez <lpaezbusiness@gmail.com>
2022-10-13 11:41:56 -0300

    Primer commit

---
+++ contador-de-clics-clase/src/componentes/Boton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../stylesheets/Boton.css"
+
+class Boton extends React.Component {
+  render() {
+      return (
+          <button
+              className={this.props.esBotonDeClic ? "boton-clic" : "boton-reiniciar"} 
+              onClick={this.props.manejarClic}>
+              {this.props.texto}
+          </button>
+      )
+  }
+}
+export default Boton;
